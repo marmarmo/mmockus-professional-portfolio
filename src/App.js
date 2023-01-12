84e041979ccf75da50a013d5cd27928b43f93974
MarMarMo <108547874+MMockus15@users.noreply.github.com>
2023-01-11 18:20:06 -0700

    moved ages folder

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@
 import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Home from "./components/Pages/Home/Home";
-import Profile from "./components/Pages/Profile/Profile";
-import Portfolio from "./components/Pages/Portfolio/Portfolio";
-import Skills from "./components/Pages/Skills/Skills";
-import ArtWork from "./components/Pages/Art/ArtWork";
-import Podcast from "./components/Pages/Podcast/Podcast";
+import Home from "./Pages/Home/Home";
+import Profile from "./Pages/Profile/Profile";
+import Portfolio from "./Pages/Portfolio/Portfolio";
+import Skills from "./Pages/Skills/Skills";
+import ArtWork from "./Pages/Art/ArtWork";
+import Podcast from "./Pages/Podcast/Podcast";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Contact from "./components/Pages/Contact/Contact";
-import Resume from "./components/Pages/Resume/Resume";
+import Contact from "./Pages/Contact/Contact";
+import Resume from "./Pages/Resume/Resume";
 
 function App() {
   // const [activeLink, setActiveLink] = useState("home");
